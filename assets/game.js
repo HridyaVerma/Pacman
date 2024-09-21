ac3b5945fa4fea1094d86dfc10d4b3c446aa9ef4
Hridya Verma <155651124+HridyaVerma@users.noreply.github.com>
2024-09-21 23:41:52 +0530

    woking on movement

--- assets/game.js
+++ assets/game.js
@@ -1,34 +1,120 @@
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
+const pacmanFrames = document.getElementById("animation");
+const ghostFrames = document.getElementById("ghosts");
 
-const pacmanframes =document.getElementById("animations");
-const ghostframes=document.getElementById9("ghosts");
-
-let createRect=(x,y ,width,height,color)=>{
+let createRect = (x, y, width, height, color) => {
     canvasContext.fillStyle = color;
-    canvasContext.fillRect(x,y, width,height);
+    canvasContext.fillRect(x, y, width, height);
 };
 
-let map =[
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-    [1,2,2,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,2,1]
-    [1,2,1,1,1,2,1,1,1,2,1,2,1,1,1,2,1,1,1,2,1]
-    [1,2,1,1,1,2,1,1,1,2,1,2,1,1,1,2,1,1,1,2,1]
-    [1,2,2,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,2,1]
-    [1,2,1,1,1,2,1,1,2,1,1,1,1,1,1,2,1,1,1,1,1]
-    [1,2,2,2,2,2,1,2,2,2,1,2,2,2,1,2,2,2,2,2,1]
-    [1,1,1,1,1,2,1,1,1,2,1,2,1,1,1,2,1,1,1,1,1]
-    [2,2,2,2,1,2,1,2,2,2,2,2,2,2,1,2,1,2,2,2,2]
-    [1,1,1,1,1,2,1,2,1,1,2,1,1,2,1,2,1,1,1,1,1]
-    [2,2,2,2,2,2,2,2,1,2,2,2,1,2,2,2,2,2,2,2,2]
-    [1,1,1,1,1,2,1,2,1,2,2,2,1,1,1,2,1,1,1,1,1]
-    [1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1]
-    [1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1]
-    [1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1]
-    [1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1]
-    [1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1]
-    [1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,2,2,2,2,2,1]
-    [1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1]
-    [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1]
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+// Game variables
+let fps = 30;
+let pacman;
+let oneBlockSize = 20;
+let wallcolor= "#342DCA"
+let wallSpaceWidth= oneBlockSize/1.5;
+let walloffset=(oneBlockSize-wallSpaceWidth)/2;
+let wallInnerColor= "black";
+
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_BOTTOM =1;
+
+
+// we now create the map of the walls,
+// if 1 wall, if 0 not wall
+// 21 columns // 23 rows
+let map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1],
+    [1, 1, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1],
+    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+
+
+let gameloop =() =>{
+    update();
+    draw();
+}
+let update=() =>
+{ //todo
+    
+}
+
+let draw = () =>
+{
+    createRect(0,0,canvas.width,canvas.height, "black");
+    drawWalls();
+    
+}
+
+
+
+// for (let i = 0; i < map.length; i++) {
+//     for (let j = 0; j < map[0].length; j++) {
+//         map[i][j] = 2;
+//     }
+// }
+
+let gameInterval = setInterval(gameloop, 1000/fps)
+
+let drawWalls = () => {
+    for( let i = 0 ;i< map.length;i++)
+    {
+        for(let j = 0; j<map[0].length;j++)
+        {
+            if(map[i][j] == 1)
+            {
+                createRect(j * oneBlockSize , i * oneBlockSize , oneBlockSize , oneBlockSize , wallcolor);
+                if( j>0 && map[i][j-1]==1){
+                    createRect(j* oneBlockSize,i *oneBlockSize+walloffset,
+                        wallSpaceWidth+walloffset,
+                        wallSpaceWidth,wallInnerColor);
+                }
+            }
+
+            if(j<map[0].length-1 && map[i][j+1] == 1)
+            {
+                createRect(j* oneBlockSize+ walloffset,i *oneBlockSize+walloffset,
+                    wallSpaceWidth+walloffset,
+                    wallSpaceWidth,wallInnerColor);
+            }
+
+            if(i>0 && map[i-1][j]==1){
+                createRect(j*oneBlockSize+walloffset,i*oneBlockSize,
+                    wallSpaceWidth,wallSpaceWidth+walloffset,wallInnerColor);
+            }
+
+            if(i<map.length-1 && map[i+1][j] ==1)
+            {
+                createRect(j*oneBlockSize+walloffset,i*oneBlockSize+walloffset,
+                    wallSpaceWidth,wallSpaceWidth+walloffset,wallInnerColor);
+            }
+            
+
+        }
+    }
+};
+
